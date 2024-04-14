b58ac9949f5aabe1fc0428d1b7a04039a29afae6
ccalobeto <calobeto.leon@gmail.com>
2024-04-14 15:33:04 -0500

    multiline in epicenter fixes #1

--- src/js/drawmap.js
+++ src/js/drawmap.js
@@ -47,7 +47,7 @@ export function mapChart (data, {
     .attr('pointer-events', 'none')
 
   // Add data
-  const circleG = innerChart
+  const circles = innerChart
     .append('g')
     .attr('class', 'map-data')
     .selectAll(null)
@@ -58,7 +58,7 @@ export function mapChart (data, {
     })
 
   // add circles
-  circleG
+  circles
     .append('circle')
     .attr('stroke', 'black')
     .attr('fill-opacity', 0.8)
@@ -66,35 +66,39 @@ export function mapChart (data, {
     .attr('fill', d => colorScale(d[colorBy])) //
     .attr('r', d => radiusScale(d[radiusBy])) //
 
-  // add legend
-  circleG
+  // add text
+  const textCircles = circles
     .append('text')
-    .attr('text-anchor', 'end')
     .style('font-size', '15px')
-    .attr('x', -40)
-    .attr('y', -30)
+    .attr('x', 0)
+    .attr('y', 0)
     .attr('stroke-width', 0.25)
+    .attr('text-anchor', 'middle')
     .attr('stroke', d => {
       return d.magnitude >= thresholdBigMagnitude
         ? 'black'
         : d.magnitude >= thresholdMidMagnitude ? '#f0f0f0' : null
     })
+
+  textCircles
+    .append('tspan')
+    .attr('y', d => -(radiusScale(d[radiusBy]) + 20))
     .text(d => {
-      const title = `M${formatted(d[radiusBy])}, ${d.year}\n ${d.description} `
+      const title = `M${formatted(d[radiusBy])}, ${d.year}`
       return d.magnitude >= thresholdMidMagnitude ? title : null
     })
 
-  // add lines guides
-  circleG.append('line')
-    .attr('stroke', d => {
-      return d.magnitude >= thresholdBigMagnitude
-        ? 'black'
-        : d.magnitude >= thresholdMidMagnitude ? 'gray' : null
+  textCircles
+    .append('tspan')
+    .attr('x', d => {
+      const title = `${d.description}, ${d.department}`
+      return -(title.length - 10)
     })
-    .attr('x1', 0)
-    .attr('y1', 0)
-    .attr('x2', -40)
-    .attr('y2', -30)
+    .attr('y', d => -(radiusScale(d[radiusBy]) + 5))
+    .text(d => {
+      const title = `${d.description}, ${d.department}`
+      return d.magnitude >= thresholdMidMagnitude ? title : null
+    })
 
   return innerChart.node()
 }
