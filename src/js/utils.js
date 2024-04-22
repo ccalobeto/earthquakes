146f34958d98bc110f82ee5f494654ab85a74ed5
ccalobeto <calobeto.leon@gmail.com>
2024-04-21 19:07:39 -0500

    fix the function to show the axis bottom

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,28 +1,27 @@
-import d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm'
-export function multiFormat () {
-  const formatMillisecond = d3.timeFormat('.%L')
-  const formatSecond = d3.timeFormat(':%S')
-  const formatMinute = d3.timeFormat('%I:%M')
-  const formatHour = d3.timeFormat('%I %p')
-  const formatDay = d3.timeFormat('%a %d')
-  const formatWeek = d3.timeFormat('%b %d')
-  const formatMonth = d3.timeFormat('%b')
-  const formatYear = d3.timeFormat('%Y')
+import { timeFormat } from 'https://cdn.jsdelivr.net/npm/d3-time-format@4.1.0/+esm'
+import { timeSecond, timeMinute, timeHour, timeDay, timeWeek, timeMonth, timeYear } from 'https://cdn.jsdelivr.net/npm/d3-time@3.1.0/+esm'
 
-  function multiFormat (date) {
-    return (d3.timeSecond(date) < date
-      ? formatMillisecond
-      : d3.timeMinute(date) < date
-        ? formatSecond
-        : d3.timeHour(date) < date
-          ? formatMinute
-          : d3.timeDay(date) < date
-            ? formatHour
-            : d3.timeMonth(date) < date
-              ? (d3.timeWeek(date) < date ? formatDay : formatWeek)
-              : d3.timeYear(date) < date
-                ? formatMonth
-                : formatYear)(date)
-  }
-  return multiFormat
+export function multiFormat (date) {
+  const formatMillisecond = timeFormat('.%L')
+  const formatSecond = timeFormat(':%S')
+  const formatMinute = timeFormat('%I:%M')
+  const formatHour = timeFormat('%I %p')
+  const formatDay = timeFormat('%a %d')
+  const formatWeek = timeFormat('%b %d')
+  const formatMonth = timeFormat('%b')
+  const formatYear = timeFormat('%Y')
+
+  return (timeSecond(date) < date
+    ? formatMillisecond
+    : timeMinute(date) < date
+      ? formatSecond
+      : timeHour(date) < date
+        ? formatMinute
+        : timeDay(date) < date
+          ? formatHour
+          : timeMonth(date) < date
+            ? (timeWeek(date) < date ? formatDay : formatWeek)
+            : timeYear(date) < date
+              ? formatMonth
+              : formatYear)(date)
 }
