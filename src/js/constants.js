5a9c489fee38d8362c6f0608512c5b2294280944
ccalobeto <calobeto.leon@gmail.com>
2024-04-25 07:06:04 -0500

    Updates

--- src/js/constants.js
+++ src/js/constants.js
@@ -11,4 +11,11 @@ export const aspectRatio = 3 / 2
 export const width = 1100
 export const height = aspectRatio * width
 export const circleLegendArr = [7, 8, 9]
-export const margin = ({ top: 0, right: 20, bottom: 0, left: 0 })
+export const margin = ({ top: 20, right: 20, bottom: 30, left: 0 })
+export const innerWidth = width - margin.left - margin.right
+export const innerHeight = height - margin.top - margin.bottom
+export const regions = ({
+  Coast: ['Lima y Callao', 'Moquegua', 'Tacna', 'Lambayeque', 'La Libertad', 'Ica', 'Piura', 'Tumbes', 'Ancash', 'Arequipa'],
+  Mountains: ['Cajamarca', 'San Martin', 'Huanuco', 'Pasco', 'Junin', 'Ayacucho', 'Huancavelica', 'Apurimac', 'Cusco', 'Puno'],
+  Jungle: ['Amazonas', 'Loreto', 'Madre De Dios', 'Ucayali']
+})
